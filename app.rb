d6c9e72b751f9a344f30860df2e93aa9e649fcbe
Anil Wadghule <anildigital@gmail.com>
2009-02-22 12:43:05 +0530

    Implemented hours, minutes and seconds in countdown

--- app.rb
+++ app.rb
@@ -2,9 +2,12 @@ require 'rubygems'
 require 'sinatra'
 
 get "/" do
-  result_day = Time.parse("22-02-2009")
-  today = Time.now
-  @days = ((result_day - today) / 86400).to_i
+  result_time = Time.parse("23-02-2009 01:00:00")
+  current_time = Time.now
+  @days = ((result_time - current_time) / 86400).to_i
+  @hours = ((result_time - current_time) / 3600).to_i
+  @minutes = ((result_time - current_time) / 60).to_i
+  @seconds = (result_time - current_time)
   erb :index
 end
 
